afcb20c4746d2ebd83bf1a04149eb5b4574560cb
Xiongyi Cui <cuixiongyi2013@gmail.com>
2016-04-16 14:33:48 -0400

    add install bash

--- install_ROS_jade_14.04.bash
+++ install_ROS_jade_14.04.bash
@@ -1,6 +1,6 @@
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 
 sudo apt-get update
 
@@ -15,31 +15,33 @@ sudo apt-get install -y python-wstool
 
 
 # install ros
-sudo apt-get install -y ros-indigo-desktop-full
+sudo apt-get install -y ros-jade-desktop-full
 #install moveit
-sudo apt-get install -y ros-indigo-moveit-full
+#sudo apt-get install -y ros-jade-moveit-full
 
 
 sudo apt-get install -y cmake python-catkin-pkg python-empy python-nose python-setuptools libgtest-dev build-essential
-sudo apt-get install -y ros-indigo-catkin
+sudo apt-get install -y ros-jade-catkin
 
-source /opt/ros/indigo/setup.bash
+source /opt/ros/jade/setup.bash
 
 sudo rosdep init
 rosdep update
 mkdir -p ~/workspace/src
 cd ~/workspace/src
+sudo chmod -R 777 ~/workspace/src/
 catkin_init_workspace
 cd ../
 catkin_make
 source devel/setup.bash
 
 echo '########## system_setup.bash  START' >> ~/.bashrc
-echo source /opt/ros/indigo/setup.bash >> ~/.bashrc
+echo source /opt/ros/jade/setup.bash >> ~/.bashrc
 echo source ~/workspace/devel/setup.bash >> ~/.bashrc
 echo alias yhome="'cd ~/workspace/src/'" >> ~/.bashrc
 echo alias ymake="'catkin_make install -DCMAKE_INSTALL_PREFIX:PATH=~/workspace/install -C ~/workspace -DCMAKE_BUILD_TYPE=Debug'" >> ~/.bashrc
-echo export ROS_PACKAGE_PATH=~/workspace/src/:/opt/ros/indigo/share:$ROS_PACKAGE_PATH >> ~/.bashrc
+echo alias ymakerelease="'catkin_make install -DCMAKE_INSTALL_PREFIX:PATH=~/workspace/install -C ~/workspace -DCMAKE_BUILD_TYPE=Release'" >> ~/.bashrc
+echo export ROS_PACKAGE_PATH=~/workspace/src/:/opt/ros/jade/share:$ROS_PACKAGE_PATH >> ~/.bashrc
 
 echo '########## system_setup.bash  END' >> ~/.bashrc
 source ~/.bashrc
--- library/install_Dependency_Library.bash
+++ library/install_Dependency_Library.bash
@@ -31,14 +31,19 @@ cd ..
 
 # Install openrave
 #tar -zxvf openrave.tar.gz 
-git clone https://github.com/rdiankov/openrave
-cd openrave/
-git checkout 9efdc1c777
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-# CMAKE_INSTALL_PREFIX: /usr/local
-sudo make install
+# install openrave 0.9
+#git clone https://github.com/rdiankov/openrave
+#cd openrave/
+#git checkout 9efdc1c777
+
+# install 0.8.2
+#git clone --branch latest_stable https://github.com/rdiankov/openrave.git
+#cd openrave/
+
+#mkdir build
+#cd build
+#cmake -DCMAKE_BUILD_TYPE=Release ..
+#sudo make install
 cd ..
 cd ..
 
@@ -53,16 +58,29 @@ cd ..
 
 
 # Install g2o
-sudo apt-get install -y libqglviewer-dev
-sudo apt-get install -y libsuitesparse-dev
-tar -zxvf g2o-master.tar.gz 
-cd g2o-master/
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release QGLVIEWER_LIBRARY_RELEASE=/usr/lib/x86_64-linux-gnu/libQGLViewer.so ..
+#sudo apt-get install -y libqglviewer-dev
+#sudo apt-get install -y libsuitesparse-dev
+#tar -zxvf g2o-master.tar.gz 
+#cd g2o-master/
+#mkdir build
+#cd build
+#cmake -DCMAKE_BUILD_TYPE=Release QGLVIEWER_LIBRARY_RELEASE=/usr/lib/x86_64-linux-gnu/libQGLViewer.so ..
 # CMAKE_INSTALL_PREFIX: /usr/local
-sudo make install
-cd ..
-cd ..
+#sudo make install
+#cd ..
+#cd ..
+
+# compile OpenCV
+#sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff4-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
+
+#tar -zxvf opencv-3.1.0.tar.gz
+#cd opencv-3.1.0
+#mkdir build
+#cd build
+#cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+#make
+#sudo make install
+#sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+#sudo ldconfig
 
 

56df7d2564a3324a124f8e129261cd769e89b6ce
Xiongyi Cui <cuixiongyi2013@gmail.com>
2016-08-21 09:13:46 -0400

    compile indigo works

--- compile_indigo.sh
+++ compile_indigo.sh
@@ -1,3 +1,6 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+
 sudo apt-get update
 sudo apt-get -y install git
 # http://wiki.ros.org/indigo/Installation/Source
@@ -13,5 +16,10 @@ wstool init -j8 src indigo-desktop-wet.rosinstall
 # If wstool init fails or is interrupted, you can resume the download by running:
 # wstool update -j 4 -t src
 # Resolving Dependencies
-rosdep install --from-paths src --ignore-src --rosdistro indigo -y
+sudo rosdep install --from-paths src --ignore-src --rosdistro indigo -y
 
+# for some reason class_loader requires libpocofoundationd, which is the debug version 
+sudo apt-get install libpocofoundation9*
+
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Debug
+source ~/ros_workspace/install_isolated/setup.bash
--- install_gcc6.bash
+++ install_gcc6.bash
@@ -1,8 +1,16 @@
 # reference 
 # http://askubuntu.com/questions/466651/how-do-i-use-the-latest-gcc-on-ubuntu
+sudo apt-get install -y software-properties-common
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install -y gcc-6 g++-6
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+
+# add alternative for 4.8
+# sudo apt-get install gcc-4.8 g++-4.8
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+
+# choose alternative
+# sudo update-alternatives --config gcc

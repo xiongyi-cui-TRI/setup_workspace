f9df89f3a15b6572f2f72056bc0c41cd0f144f91
Xiongyi Cui <cuixiongyi2013@gmail.com>
2017-06-28 20:48:06 -0400

    fix bug

--- python/getRobotPackageName.py
+++ python/getRobotPackageName.py
@@ -2,28 +2,31 @@ import cmdUtil
 from plumbum.cmd import rospack
 
 def getRobotPackageName():
-	cmd = rospack['list']
-	ret = cmdUtil.runCmd(cmd)
+    cmd = rospack['list']
+    ret = cmdUtil.runCmd(cmd)
     if not ret[0]:
-    	return []
-	stdout = ret[2]
-	packs = stdout.split('\n')
-	robotPackages = []
-	for pack in packs:
-		# pack example:
-		# kuka_110_moveit_config /opt/ros/indigo/share/kuka_110_moveit_config
-		moveitSuffix = '_moveit_config'
-		if moveitSuffix in pack:
-			# packName example:
-			# kuka_110
-			packName = pack.split(' ')[0].replace(moveitSuffix, '')
-			candidate = packName
-			if '\n'+candidate+' ' in stdout:
-				robotPackages.append(candidate)
-	return robotPackages
+        return []
+    stdout = ret[2]
+    packs = stdout.split('\n')
+    robotPackages = []
+    for pack in packs:
+        # pack example:
+        # kuka_110_moveit_config /opt/ros/indigo/share/kuka_110_moveit_config
+        moveitSuffix = '_moveit_config'
+        if moveitSuffix in pack:
+            # packName example:
+            # kuka_110
+            packName = pack.split(' ')[0].replace(moveitSuffix, '')
+            candidate = packName
+            if '\n'+candidate+' ' in stdout:
+                robotPackages.append(candidate)
+    return robotPackages
 
+def main():
+    print(getRobotPackageName())
 
-
+if __name__ == '__main__':
+    main()
 
 
 

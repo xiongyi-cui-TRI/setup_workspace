79f3ef09a0ad324273d2f4035c2a04b48654a76e
Xiongyi Cui <cuixiongyi2013@gmail.com>
2017-07-28 22:16:03 -0400

    add install gitflow

--- python/installSoftware.py
+++ python/installSoftware.py
@@ -5,6 +5,7 @@ import apt_get
 import sysUtil
 from plumbum.cmd import sudo
 from plumbum.cmd import tee
+from plumbum.cmd import cp
 from plumbum.cmd import echo
 from plumbum.cmd import ls
 
@@ -40,14 +41,17 @@ class chrome:
     def install():
         apt_get.apt_get_install('google-chrome-stable')
 
+
 class simpleRecorder:
     @staticmethod
     def config():
-        os.system('sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder')
+        os.system(
+            'sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder')
 
     @staticmethod
     def install():
         apt_get.apt_get_install('simplescreenrecorder')
+
 
 class mono:
     @staticmethod
@@ -63,100 +67,97 @@ class mono:
     def install():
         apt_get.apt_get_install('mono-devel')
 
+
 class clang:
-    clangVersion = '4.0'    
+    # clangVersion = '4.0'
     @staticmethod
     def config():
         a = 0
         # NOTE: Not using This method, using the default Ubuntu repo to install clang 3.9
-                # install clang 4.0
-                # http://apt.llvm.org/
-                # os.system('wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -')
-                # srcDir="/etc/apt/sources.list.d/"
-                # srcFile=srcDir+'clang.list'
 
-                # if os.path.isfile(srcFile):
-                #     return
-                # os.system("sudo mkdir -p " + srcDir)
+    # install clang 4.0
+    # http://apt.llvm.org/
+    # os.system('wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -')
+    # srcDir="/etc/apt/sources.list.d/"
+    # srcFile=srcDir+'clang.list'
 
-                # teeToSrc = 'sudo tee --append '+srcFile+''
-                # if sysUtil.isUbuntu14LTS():
-                #     os.system("echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-"+ \
-                #         clang.clangVersion+" main\n \
-                #         deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-"+clang.clangVersion
-                #         +" main' | " + teeToSrc)
-                # elif sysUtil.isUbuntu16LTS():
-                #     os.system("echo 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-"+ \
-                #         clang.clangVersion+" main\n \
-                #         deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-"+clang.clangVersion
-                #         +" main' | " + teeToSrc)
+    # if os.path.isfile(srcFile):
+    #     return
+    # os.system("sudo mkdir -p " + srcDir)
+
+    # teeToSrc = 'sudo tee --append '+srcFile+''
+    # if sysUtil.isUbuntu14LTS():
+    #     os.system("echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-"+ \
+    #         clang.clangVersion+" main\n \
+    #         deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-"+clang.clangVersion
+    #         +" main' | " + teeToSrc)
+    # elif sysUtil.isUbuntu16LTS():
+    #     os.system("echo 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-"+ \
+    #         clang.clangVersion+" main\n \
+    #         deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-"+clang.clangVersion
+    #         +" main' | " + teeToSrc)
 
     @staticmethod
     def install():
         clang.clangVersion = '3.9'
-        apt_get.apt_get_install(
-            [
-            'clang-'+clang.clangVersion,
-            'clang-format-'+clang.clangVersion,
-            'libfuzzer-'+clang.clangVersion+'-dev'
-            ])
+        apt_get.apt_get_install([
+            'clang-' + clang.clangVersion,
+            'clang-format-' + clang.clangVersion,
+            'libfuzzer-' + clang.clangVersion + '-dev'
+        ])
         os.system('sudo rm -rf /usr/bin/llvm-symbolizer')
-        os.system('sudo ln -s /usr/bin/llvm-symbolizer-'+clang.clangVersion+' /usr/bin/llvm-symbolizer')
+        os.system('sudo ln -s /usr/bin/llvm-symbolizer-' + clang.clangVersion +
+                  ' /usr/bin/llvm-symbolizer')
         # NOTE export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
         # this is done in export_path.py
+
 
 class git:
     @staticmethod
     def config():
-        return 
-    
+        os.system('https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash')
+        cmd = sudo[cp['git-flow-completion.bash', '/etc/bash-completion.d/']]
+        ret = cmdUtil.runCmd(cmd)
+        return
+
     @staticmethod
     def install():
+        apt_get.apt_get_install(['git', 'gitk', 'git-flow'])
+        return
 
-        return 
 
 class tools:
     @staticmethod
     def config():
-        return 
-    
+        return
+
     @staticmethod
     def install():
-        apt_get.apt_get_install(
-        ['build-essential',
-        'git',
-        'gitk',
-        'meld',
-        'vim',
-        'terminator',
-        'htop',
-        'xclip',
-        'openssh-server',
-        'sshpass',
-        'cmake',
-        'libgtest-dev'
-
+        apt_get.apt_get_install([
+            'build-essential',
+            'bash-completion',
+            'meld',
+            'vim',
+            'terminator',
+            'htop',
+            'xclip',
+            'openssh-server',
+            'sshpass',
+            'cmake',
+            'libgtest-dev',
         ])
-        return 
+        return
 
 
 # install Git-Extension
 # https://github.com/gitextensions/gitextensions/releases/tag/v2.49.03
 
 if __name__ == '__main__':
-    installList = [
-    sublime,
-    chrome,
-    mono,
-    simpleRecorder,
-    clang,
-    tools
-    ]
+    installList = [sublime, chrome, mono, simpleRecorder, clang, tools]
     for t1 in installList:
         t1.config()
-    
+
     apt_get.apt_get_update()
 
     for t1 in installList:
         t1.install()
-    

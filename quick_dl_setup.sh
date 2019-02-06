f4a08c1f2dfd1eae91ba9dee6ae8f65d24adbc9e
Xiongyi Cui <cuixiongyi2013@gmail.com>
2019-02-06 10:42:14 -0500

    append setup script into .bashrc

--- quick_dl_setup.sh
+++ quick_dl_setup.sh
@@ -4,10 +4,10 @@ echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/source
 
 sudo apt-get update
 
-sudo apt-get install git vim htop gitk sublime-text gnome-tweak-tool
+sudo apt-get install -y git vim htop gitk sublime-text gnome-tweak-tool
 
 
-sudo apt-get install python3-pip python3-venv python3-yapf python3-tk
+sudo apt-get install -y python3-pip python3-venv python3-yapf python3-tk
 
 mkdir -p ~/workspace
 cd ~/workspace
@@ -16,17 +16,22 @@ python3 -m venv dl_env
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# put into ~/.bashrc
-	# Avoid duplicates
-	export HISTCONTROL=ignoredups:erasedups  
-	# When the shell exits, append to the history file instead of overwriting it
-	shopt -s histappend
+# put setup script into .bashrc
+echo "# xiongyi workspace setup script start----------
+        # record command history, avoid duplicates
+        export HISTCONTROL=ignoredups:erasedups  
+        # When the shell exits, append to the history file instead of overwriting it
+        shopt -s histappend
 
-	# After each command, append to the history file and reread it
-	export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
-	function activate_dl_env 
-	{
-		source ~/workspace/dl_env/bin/activate
-	}
+        # After each command, append to the history file and reread it
+        export PROMPT_COMMAND=\"\${PROMPT_COMMAND:+\$PROMPT_COMMAND$'\\n'}history -a; history -c; history -r\"
+        function activate_dl_env 
+        {
+                source ~/workspace/dl_env/bin/activate
+        }
+# xiongyi workspace setup script end----------
+" | tee -a ~/.bashrc
 
+# install libraries in venv
+source ~/workspace/dl_env/bin/activate
 pip3 install torch torchvision numpy scipy matplotlib ipython jupyter pandas
